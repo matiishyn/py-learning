fe0f4649324fade8627c8eabf11ddc8de7a94cb1
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-04-23 13:51:16 +0200

    vars

---
+++ 1-basics/2-vars.py
@@ -0,0 +1,30 @@
+# space in memory
+name = 'ivan'
+_validVar = 1
+Valid_name = 2
+another123 = 123
+
+age = 25
+# print("I'm " + age)  # TypeError: must be str, not int
+print("I'm " + str(age))
+
+# DATA TYPES
+# numeric
+# int
+integer = 123
+floating = 2.5
+
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)  # to integer
+print(a % b)
+print((a + b) / (3 - 4) * 12)
+print()
+
+for i in range(1, a // b):
+    print(i)
+print()
--- 1-basics/readme.md
+++ 1-basics/readme.md
@@ -3,3 +3,14 @@
 Python is an object oriented interpreted programming language.
 Runs on Mac, Linux, Windows.
 Data is strongly typed,
+
+## DataTypes
+* numeric
+    - integer
+    - float
+* sequences
+* mappings
+* files
+* classes
+* instances
+* exceptions
