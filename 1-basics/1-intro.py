7eeb5a963e72a0f01c0a9dd96f2dd6d65f570da6
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-04-23 12:58:40 +0200

    intro

---
+++ 1-basics/1-intro.py
@@ -0,0 +1,44 @@
+__author__ = 'ivan'
+
+# using 'print' function to print a string, numbers
+print('Hello, World!')
+print(1 + 2)
+print()
+
+# Quotes
+print('Single quote')
+print("Double quote")
+print()
+
+# Concat strings
+print('hello ' + 'world')
+print()
+
+# Vars
+name = 'Ivan'
+print('hello, ' + name)
+
+# Input
+name = input('what is your name?')
+print('Hello, ' + name)
+print()
+
+# Special chars
+print('one\nsec\nand third line')
+print('1\t2\t3')
+print('Escaping \' quotes')
+print("Escaping \" quotes")
+print()
+
+# triple quotes
+print("""This
+is 
+another
+split 
+string""")
+print('''in here we don't need to escape quotes ' or ".''')
+print()
+print()
+print()
+print()
+print()
---
+++ 1-basics/readme.md
@@ -0,0 +1,5 @@
+# basics
+
+Python is an object oriented interpreted programming language.
+Runs on Mac, Linux, Windows.
+Data is strongly types, 
