8f1cc346edf7547c913bfc1212a2e5c1cdade1b0
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-05-01 22:55:54 +0200

    range

--- 3-sequence-types/4-range.py
+++ 3-sequence-types/4-range.py
@@ -1,6 +1,8 @@
 l = range(10)
 print(list(l))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print('==============================================')
+
+print('=' * 50)
+# ===============================================================
 
 s = 'abcdefghijklmnopqrstuvwxyz'
 print(s.index('e'))
@@ -11,14 +13,51 @@ r = range(1, 100, 2)
 print(r.index(7))
 print(r[3])
 print(7 in r)  # True - check if exists in range
-print('==============================================')
+
+print('=' * 50)
+# ===============================================================
 
 r1 = range(1, 10)
 r2 = r1[::2]
 print(r2)
-print('==============================================')
+
+print('=' * 50)
+# ===============================================================
 
 decimals = range(0, 100)
 r1 = decimals[3:40:3]
 r2 = range(3, 40, 3)
 print(r1 == r2)  # True - equals
+
+print('=' * 50)
+# ===============================================================
+
+r1 = range(0, 5, 2)  # [0, 2, 4]
+r2 = range(0, 6, 2)  # [0, 2, 4]
+print(r1 == r2)  # True - equals
+
+print('=' * 50)
+
+r = range(1, 10)
+for i in r[::-2]:
+    print(i)  # 9 7 5 3 1
+
+print()
+# SAME AS
+
+for i in range(9, 0, -2):
+    print(i)  # 9 7 5 3 1
+
+print()
+
+print(range(1, 10)[::-2] == range(9, 0, -2))  # True
+
+print('=' * 50)
+# ===============================================================
+
+backStr = 'nohtyP'
+print(backStr[::-1])  # printed backward
+
+r = range(0, 10)
+for i in r[::-1]:
+    print(i)  # 9 8 7 6 5 4 3 2 1 0
