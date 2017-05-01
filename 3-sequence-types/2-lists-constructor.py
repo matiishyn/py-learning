5927b5c89bd69df9fcff23b2ad512ca23e5f2fbd
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-05-01 20:53:33 +0200

    lists

--- 3-sequence-types/2-lists-constructor.py
+++ 3-sequence-types/2-lists-constructor.py
@@ -5,3 +5,12 @@ if l1 == l2:  # TRUE
     print('{} and {} are equal'.format(l1, l2))
 
 print(list('a list'))
+print('==============================================')
+
+list1 = [1, 2, 3, 4, 5]
+list2 = list1
+
+list2.sort(reverse=True)
+print(list1)
+print(list2)
+print(list1 == list1)  # TRUE - both lists are changed
